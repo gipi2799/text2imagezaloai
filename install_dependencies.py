ca201744827ef402d301d6a69cd619210797d561
gipi2799 <146510988+gipi2799@users.noreply.github.com>
2024-01-09 16:32:38 +0700

    Update install_dependencies.py

--- install_dependencies.py
+++ install_dependencies.py
@@ -1,6 +1,6 @@
-!pip install keras-cv
-!pip install git+https://github.com/keras-team/keras-cv.git tensorflow --upgrade
-!pip install -U tensorflow -q
-!pip install keras-core -q
-!pip install -U deep-translator
-!pip install -U tensorflow[and-cuda] -q
+pip install keras-cv
+pip install git+https://github.com/keras-team/keras-cv.git tensorflow --upgrade
+pip install -U tensorflow -q
+pip install keras-core -q
+pip install -U deep-translator
+pip install -U tensorflow[and-cuda] -q
